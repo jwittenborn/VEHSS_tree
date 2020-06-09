6631c1907510d52a49f24cb145214349a814589f
jwittenborn <42275422+jwittenborn@users.noreply.github.com>
2020-06-09 17:47:48 -0400

    commit 2

---
+++ treeData.js
@@ -0,0 +1,270 @@
+var treeData =				
+{				
+	"name": "Conditions (claims)",			
+	"children": [			
+				
+		{		
+		"name": "Retinal Detachment and Defects",		
+		"children": [		
+			{ "name": "Retinal detachment and defects" },	
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		]		
+		},		
+		//  END Retinal Detachment and Defects		
+				
+				
+		{		
+		"name": "Diabetic Eye Diseases",		
+		"children": [		
+			{ "name": "Early/mild diabetic retinopathy" },	
+			{ "name": "Moderate/Severe non-proliferative diabetic retinopathy" },	
+			{ "name": "Proliferative diabetic retinopathy" },	
+			{ "name": "Diabetic macular edema" },	
+			{ "name": "Other diabetes related eye disorders" },	
+			{ "name": "All Diabetic eye diseases" },	
+				
+				
+				
+				
+				
+				
+		]		
+		},		
+		//  END Diabetic Eye Diseases		
+				
+				
+				
+		{		
+		"name": "Age Related Macular Degeneration",		
+		"children": [		
+			{ "name": "Unspecified-age related macular degeneration" },	
+			{ "name": "Early stage age-related macular degeneration" },	
+			{ "name": "Dry-form age-related macular degeneration" },	
+			{ "name": "Geographic atrophy" },	
+			{ "name": "Wet-form age-related macular degeneration" },	
+			{ "name": "Choroidal neovascularization" },	
+			{ "name": "All Age-related macular degeneration (AMD)" },	
+				
+				
+				
+				
+				
+		]		
+		},		
+		//  END Age Related Macular Degeneration		
+				
+		{		
+		"name": "Other Retinal Disorders",		
+		"children": [		
+			{ "name": "Other/unspecified visual disturbances" },	
+			{ "name": "Occlusive retinal vascular disease" },	
+			{ "name": "Other/unspecified other retinal disorders" },	
+			{ "name": "Retinopathy of prematurity" },	
+			{ "name": "Macular edema (Cystoid or non-diabetic)" },	
+			{ "name": "Central retinal vein occlusion" },	
+			{ "name": "Branch retinal vein occlusion" },	
+			{ "name": "Central retinal arterial occlusion" },	
+			{ "name": "Branch retinal artery occlusion" },	
+			{ "name": "Non-Occlusive retinal vascular disease" },	
+			{ "name": "Hereditary chorioretinal dystrophy" },	
+			{ "name": "Myopic degeneration" },	
+			{ "name": "All Other retinal disorders" },	
+		]		
+		},		
+		//  END Other Retinal Disorders		
+				
+		{		
+		"name": "Glaucoma",		
+		"children": [		
+			{ "name": "Open-angle glaucoma" },	
+			{ "name": "Secondary glaucoma" },	
+			{ "name": "Low-tension glaucoma" },	
+			{ "name": "Glaucoma suspect" },	
+			{ "name": "Primary angle-closure glaucoma" },	
+			{ "name": "Congenital glaucoma" },	
+			{ "name": "Other/unspecified glaucoma" },	
+			{ "name": "All Glaucoma" },	
+				
+				
+				
+		]		
+		},		
+		//  END Glaucoma		
+				
+				
+		{		
+		"name": "Cataracts",		
+		"children": [		
+			{ "name": "Congenital Cataract" },	
+			{ "name": "Posterior capsular opacity" },	
+			{ "name": "Pseudophakia" },	
+			{ "name": "Aphakia and disorders of lens" },	
+			{ "name": "Age-related cataract" },	
+			{ "name": "Other or unspecified cataract" },	
+			{ "name": "All Cataracts" },	
+				
+				
+				
+				
+		]		
+		},		
+		//  END Cataracts		
+				
+		{		
+		"name": "Disorders of Refraction and Accommodation",		
+		"children": [		
+			{ "name": "Myopia" },	
+			{ "name": "Hypermetropia" },	
+			{ "name": "Astigmatism" },	
+			{ "name": "Presbyopia" },	
+			{ "name": "Other refraction and accommodation disorders" },	
+			{ "name": "All Refraction and accommodation disorders" },	
+				
+				
+				
+				
+				
+		]		
+		},		
+		//  END Disorders of Refraction and Accommodation		
+				
+		{		
+		"name": "Diagnosed Blindness and Low Vision",		
+		"children": [		
+			{ "name": "Unqualified impairment, both eyes" },	
+			{ "name": "Blindness, both eyes, including legal blindness" },	
+			{ "name": "Vision impairment one eye" },	
+			{ "name": "Moderate imp. better eye, profound imp. lesser eye" },	
+			{ "name": "Moderate bilateral impairment" },	
+			{ "name": "Profound bilateral imp., legal blindness" },	
+			{ "name": "Unqualified impairment in one eye, or unspecified" },	
+			{ "name": "Low vision or blindness, one eye" },	
+			{ "name": "Blindness one eye, low vision other eye" },	
+			{ "name": "Low vision, both eyes" },	
+			{ "name": "All Blindness and low vision" },	
+		]		
+		},		
+		//  END Diagnosed Blindness and Low Vision		
+		{		
+		"name": "Strabismus and Amblyopia",		
+		"children": [		
+			{ "name": "Strabismus" },	
+			{ "name": "Amblyopia" },	
+			{ "name": "All Strabismus and amblyopia" },	
+				
+		]		
+		},		
+		//  END Strabismus and Amblyopia		
+				
+		{		
+		"name": "Injury, Burns and Surgical Complications of the Eye",		
+		"children": [		
+			{ "name": "Ocular injury" },	
+			{ "name": "Ocular burns" },	
+			{ "name": "Surgical complication of the eye" },	
+			{ "name": "All Injury, burns and surgical complications of the eye" },	
+		]		
+		},		
+		//  END Injury, Burns and Surgical Complications of the Eye		
+				
+		{		
+		"name": "Disorders of Optic Nerve and Visual Pathways",		
+		"children": [		
+			{ "name": "Optic nerve disorders" },	
+			{ "name": "Disorders of the visual pathway and visual cortex" },	
+			{ "name": "All Disorders of optic nerve and visual pathways" },	
+				
+		]		
+		},		
+		//  END Disorders of Optic Nerve and Visual Pathways		
+				
+		{		
+		"name": "Other Visual Disturbances",		
+		"children": [		
+			{ "name": "Visual field defect" },	
+			{ "name": "Color blindness" },	
+			{ "name": "Night blindness" },	
+			{ "name": "All Other visual disturbances" },	
+		]		
+		},		
+		//  END Other Visual Disturbances		
+				
+				
+		{		
+		"name": "Infectious and Inflammatory Diseases",		
+		"children": [		
+			{ "name": "Infectious diseases" },	
+			{ "name": "Keratitis" },	
+			{ "name": "Conjunctivitis" },	
+			{ "name": "Eyelid infection and inflammation" },	
+			{ "name": "Other/unspecified infectious and inflammatory diseases" },	
+			{ "name": "Lacrimal and orbit inflammation" },	
+			{ "name": "Endophthalmitis" },	
+			{ "name": "All Infectious and inflammatory diseases" },	
+				
+		]		
+		},		
+		//  END Infectious and Inflammatory Diseases		
+				
+		{		
+		"name": "Orbital and External Disease",		
+		"children": [		
+			{ "name": "Congenital anomalies" },	
+			{ "name": "Other/unspecified orbital and external disease" },	
+			{ "name": "Lacrimal diseases" },	
+			{ "name": "Eyelid disorders" },	
+			{ "name": "Dry eye syndrome" },	
+			{ "name": "Disorders of the globe" },	
+			{ "name": "All Orbital and external disease" },	
+				
+				
+		]		
+		},		
+		//  END Orbital and External Disease		
+				
+		{		
+		"name": "Cancer and Neoplasms of the Eye",		
+		"children": [		
+			{ "name": "Malignant neoplasm" },	
+			{ "name": "Benign neoplasm" },	
+			{ "name": "All Cancer and neoplasms of the eye diseases" },	
+				
+		]		
+		},		
+		//  END Cancer and Neoplasms of the Eye		
+				
+		{		
+		"name": "Cornea Disorders",		
+		"children": [		
+			{ "name": "Keratoconus" },	
+			{ "name": "Endothelial dystrophy (Fuchs)" },	
+			{ "name": "Other corneal disorders" },	
+			{ "name": "All Cornea disorders" },	
+		]		
+		},		
+		//  END Cornea Disorders		
+				
+				
+		{		
+		"name": "Other Eye Disorders",		
+		"children": [		
+			{ "name": "Other eye disorders" },	
+				
+				
+				
+		]		
+		},		
+		//  END Other Eye Disorders		
+				
+	]			
+};				
